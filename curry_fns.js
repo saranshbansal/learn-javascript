806868744df9959ea74549180eaaf77eb1dd35f9
Saransh Bansal <saran3h.bansal@gmail.com>
2019-11-06 16:06:42 +0530

    curry fns

--- curry_fns.js
+++ curry_fns.js
@@ -6,15 +6,20 @@ const myCurryFn = function(x) {
   };
 };
 
-const result = myCurryFn(1)(2)(3);
+// OR Shorthand::
+const myCurryFn2 = a => b => c => a + b + c;
 
+const result = myCurryFn(1)(2)(3);
 console.log(result);
+
+const result2 = myCurryFn2(1)(2)(3);
+console.log(result2);
 
 // Reusable functions with currying
 function add10() {
   return function(x, y) {
     return x + y + 10;
   };
-};
+}
 
 console.log(add10()(100, 120));
