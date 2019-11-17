7e3fba1d40fc2acfb7bcc1889434fa4a95175783
Saransh Bansal <saran3h.bansal@gmail.com>
2019-11-17 15:29:38 +0530

    added few new questions

---
+++ programming/guess_the_output.js
@@ -0,0 +1,39 @@
+var res = (function(
+    x,
+    f = () => {
+      return x;
+    }
+  ) {
+    var x;
+    var y = x;
+    x = "B";
+    return [f(x), y, f()];
+  })("A");
+  
+  console.log("1. ", res);
+  
+  // -----------
+  
+  const v = "abc";
+  function f(fun = x => v) {
+    const v = "xyz";
+    console.log("2. ", fun());
+  }
+  
+  f();
+  
+  // -----------
+  const a = [];
+  for (var i of ["a", "b", "c"]) {
+    a.push(() => i);
+  }
+  
+  for (const i of [1, 2, 3]) {
+    a.push(() => i);
+  }
+  
+  console.log(
+    "4. ",
+    a.map(x => x())
+  );
+  
